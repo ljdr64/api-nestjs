9edd68c894e98cffb924a8d43ef8130bbd330474
Laureano Rodriguez <laure.jd0101@gmail.com>
2024-07-23 07:37:36 -0300

    Creando Data Transfers Objects

--- src/services/products.service.ts
+++ src/services/products.service.ts
@@ -1,4 +1,5 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
+import { CreateProductDto, UpdateProductDto } from 'src/dtos/products.dtos';
 
 import { Product } from 'src/entities/product.entity';
 
@@ -28,7 +29,7 @@ export class ProductsService {
     return product;
   }
 
-  create(payload: any) {
+  create(payload: CreateProductDto) {
     this.counterId = this.counterId + 1;
     const newProduct = {
       id: this.counterId,
@@ -38,7 +39,7 @@ export class ProductsService {
     return newProduct;
   }
 
-  update(id: number, payload: any) {
+  update(id: number, payload: UpdateProductDto) {
     const product = this.findOne(id);
     if (product) {
       const index = this.products.findIndex((item) => item.id === id);
